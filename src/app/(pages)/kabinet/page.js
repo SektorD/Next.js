b38fda70d41f976ae02b85e40a8e4626d2e995f2
Muhamed Dadozhonov <92201434+SektorD@users.noreply.github.com>
2024-01-27 13:51:18 +0500

    Add files via upload

---
+++ src/app/(pages)/kabinet/page.js
@@ -0,0 +1,24 @@
+import styles from "./page.module.css";
+import Menu from "@/components/Menu/Menu";
+
+export const metadata = {
+  title: 'Кабинет',
+}
+
+export default function kabinet() {
+  return (
+    <main className={styles.main}>
+      <Menu />
+
+      <div className={styles.center}>
+        Кабинет
+      </div>
+
+      <form>
+        <input></input>
+        <input></input>
+        <button>Войти</button>
+      </form>
+    </main>
+  );
+}
